8a767ac8b446ba39f2c2a32abf994a2d1f356aeb
Marcelo Rodrigues <85708828+MMarcelo-Rodrigues@users.noreply.github.com>
2023-04-03 23:57:59 -0300

    teste de next13

---
+++ web-mmdev/.next/static/chunks/app/layout.js
@@ -0,0 +1,28 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["app/layout"],{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5CMarcelo%5CDocuments%5CGitHub%5CWeb-MMdev%5Cweb-mmdev%5Csrc%5Capp%5Cglobals.css&server=false!":
+/*!*************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5CMarcelo%5CDocuments%5CGitHub%5CWeb-MMdev%5Cweb-mmdev%5Csrc%5Capp%5Cglobals.css&server=false! ***!
+  \*************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("(() => __webpack_require__.e(/*! import() */ \"_app-client_src_app_globals_css\").then(__webpack_require__.bind(__webpack_require__, /*! ./src/app/globals.css */ \"(app-client)/./src/app/globals.css\")))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1mbGlnaHQtY2xpZW50LWVudHJ5LWxvYWRlci5qcz9tb2R1bGVzPUMlM0ElNUNVc2VycyU1Q01hcmNlbG8lNUNEb2N1bWVudHMlNUNHaXRIdWIlNUNXZWItTU1kZXYlNUN3ZWItbW1kZXYlNUNzcmMlNUNhcHAlNUNnbG9iYWxzLmNzcyZzZXJ2ZXI9ZmFsc2UhLmpzIiwibWFwcGluZ3MiOiJBQUFBLE9BQU8sK0xBQXNIIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8/ZmU2YiJdLCJzb3VyY2VzQ29udGVudCI6WyIoKCkgPT4gaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImxhenlcIiAqLyBcIkM6XFxcXFVzZXJzXFxcXE1hcmNlbG9cXFxcRG9jdW1lbnRzXFxcXEdpdEh1YlxcXFxXZWItTU1kZXZcXFxcd2ViLW1tZGV2XFxcXHNyY1xcXFxhcHBcXFxcZ2xvYmFscy5jc3NcIikpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5CMarcelo%5CDocuments%5CGitHub%5CWeb-MMdev%5Cweb-mmdev%5Csrc%5Capp%5Cglobals.css&server=false!\n"));
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["main-app"], function() { return __webpack_exec__("(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5CMarcelo%5CDocuments%5CGitHub%5CWeb-MMdev%5Cweb-mmdev%5Csrc%5Capp%5Cglobals.css&server=false!"); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
